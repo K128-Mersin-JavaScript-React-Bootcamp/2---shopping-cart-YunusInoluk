b49bef821fa1f1d1c4cd043352e954108c1bb486
Yunus Inoluk <yunusinoluk@gmail.com>
2021-10-06 20:15:33 +0300

    Category filters added

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,3 +1,4 @@
+import { tsCallSignatureDeclaration } from "@babel/types";
 import { Layout, List, Checkbox } from "antd";
 
 import { useEffect, useState } from "react";
@@ -5,6 +6,10 @@ const { Content, Sider } = Layout;
 export default function Shop() {
   const [products, setProducts] = useState([]);
   const [categoryList, setCategoryList] = useState([]);
+  const [isAllFiltersCleared, setIsAllFiltersCleared] = useState(true);
+  const [filter, setFilter] = useState({});
+  const [selectedSort, setSelectedSort] = useState("mostPopular");
+
   useEffect(() => {
     fetch("https://api.npoint.io/2443c7a7d9e0b37aec99/products")
       .then((res) => res.json())
@@ -23,6 +28,36 @@ export default function Shop() {
     setCategoryList(categorySet);
     console.log(categorySet);
   };
+  const handleCheckbox = ({ target }) => {
+    const filterName = target.name;
+    const isChecked = target.checked;
+    const newFilter = { ...filter, [filterName]: isChecked };
+    let isAllFiltersCleared = true;
+    for (const filter in newFilter) {
+      if (newFilter[filter]) {
+        isAllFiltersCleared = false;
+      }
+    }
+    setIsAllFiltersCleared(isAllFiltersCleared);
+    setFilter(newFilter);
+    console.log(newFilter);
+  };
+  const handleFilterData = () => {
+    let defaultProductList;
+    if (isAllFiltersCleared) {
+      defaultProductList = products;
+    } else {
+      defaultProductList = products.filter(
+        (product) => filter[product.category]
+      );
+      console.log("Done");
+    }
+    return defaultProductList;
+  };
+  const handleCheckedItem = (categoryName) => {
+    return filter[categoryName];
+  };
+
   return (
     <Layout>
       <Sider theme={"light"} style={{ flex: "0 0 300px", maxWidth: "300px" }}>
@@ -48,15 +83,24 @@ export default function Shop() {
           dataSource={categoryList}
           renderItem={(item) => (
             <List.Item>
-              <Checkbox name={item}>{item}</Checkbox>
+              <Checkbox
+                name={item}
+                onChange={handleCheckbox}
+                checked={handleCheckedItem(item)}
+              >
+                {item}
+              </Checkbox>
             </List.Item>
           )}
         />
       </Sider>
       <Content style={{ padding: "3rem" }}>
         <List
-          grid={{ gutter: 16, column: 4 }}
-          dataSource={products}
+          grid={{
+            gutter: 16,
+            xs: 1,
+          }}
+          dataSource={handleFilterData()}
           renderItem={(item) => (
             <List.Item>
               <div className="card">
